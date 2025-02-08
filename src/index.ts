34af587e790d8f096203531d69deaa9f9b7994b7
Sahil Shaikh <sahilshaikh8264@gmail.com>
2025-02-08 20:41:23 +0000

    Change default server port from 6000 to 5000

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const startServer = async () => {
     await connectDB();
 
     // Start the server
-    const PORT = process.env.PORT || 6000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server is running at http://localhost:${PORT}`);
     });
